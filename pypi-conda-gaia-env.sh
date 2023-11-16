78486d9c82e035bbe76c581cdf3364cda8ff743d
Jean-Guillaume Isabelle <jgi@jgwill.com>
2023-11-16 13:11:47 -0500

    fix

--- pypi-conda-gaia-env.sh
+++ pypi-conda-gaia-env.sh
@@ -1,6 +1,6 @@
 cversion=$(cat pyproject.toml |tr '"' " " |awk '/version/ {print $3}')
 conda activate jgtpy-pypi && sleep 1 && echo "... Waiting before we install the fresh package $cversion" && sleep 2 && echo "..." && sleep 4 && \
-	pip uninstall -y jgtpy && \
+	pip uninstall -y jgtpy && sleep 3 && \
 	echo "pip install -U jgtpy==$cversion" && sleep 1 && pip install -U jgtpy==$cversion && \
 	echo "------ New version should be installed ----" && \
 	echo " Entering ./test" && \
