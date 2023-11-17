28533b421951bc5a2cc174749b3949d3f7b5f0f9
JGWill Gaia.WSL <jgi@jgwill.com>
2023-11-17 14:02:54 -0500

    v2

--- docs/jgtsnoter.py
+++ docs/jgtsnoter.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import os
 import urllib.parse
 import hashlib
+import markdown
 
 # Create directory if not exists
 if not os.path.exists('./_snote_content_cache'):
@@ -11,13 +12,18 @@ if not os.path.exists('./_snote_content_cache'):
 
 with open('jgtsnoter.csv', 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
-    links = []
+    link_titles = []
 
     for row in csv_reader:
         url = row[0]
-        links.append(url)
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Extract the title of the page
+        title = soup.title.string if soup.title else url
+
+        # Store the link and its title
+        link_titles.append((url, title))
 
         # Create a hash of the URL to use as the filename
         url_hash = hashlib.md5(url.encode()).hexdigest()
@@ -37,6 +43,12 @@ with open('jgtsnoter.csv', 'r') as csv_file:
 
     # Create index-snote.md
     with open('index-snote.md', 'w') as md_file:
-        for link in links:
+        for link, title in link_titles:
             url_hash = hashlib.md5(link.encode()).hexdigest()
-            md_file.write(f'- [{link}](./_snote_content_cache/{url_hash}.html)\n')
+            md_file.write(f'- [{title}](./_snote_content_cache/{url_hash}.html)\n')
+    # Convert index-snote.md to index-snote.html
+    with open('index-snote.md', 'r') as md_file:
+        html = markdown.markdown(md_file.read())
+
+    with open('index-snote.html', 'w') as html_file:
+        html_file.write(html)
