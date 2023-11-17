6cf2e30c634287303b92a29830030e5613768989
Jean-Guillaume Isabelle <jgi@jgwill.com>
2023-11-17 15:58:26 -0500

    refacto

--- docs/v2jgtsnoter.py
+++ docs/v2jgtsnoter.py
@@ -17,6 +17,55 @@ import snoterupdate
 import os
 from dotenv import load_dotenv
 
+
+model_to_use="text-davinci-002"
+
+
+
+
+    
+def save_html_content(url_hash, soup):
+    with open(f'./_snote_content_cache/{url_hash}.html', 'w') as html_file:
+        html_file.write(soup.prettify())
+
+def download_images(soup, url):
+    for img in soup.find_all('img'):
+        img_url = urllib.parse.urljoin(url, img['src'])
+        img_filename = hashlib.md5(img_url.encode()).hexdigest()
+        img_response = requests.get(img_url)
+
+        with open(f'./_snote_content_cache/{img_filename}', 'wb') as img_file:
+            img_file.write(img_response.content)
+
+def clean_url_response2text(response):
+    soup = BeautifulSoup(response.text, 'html.parser')
+    clean_text = soup.get_text(separator=' ')
+    return clean_text[:5000]
+
+def get_summary(response, temperature=0.5):
+    prompt_text = clean_url_response2text(response)
+    completion = client.completions.create(
+        model=model_to_use,
+        prompt=prompt_text,
+        temperature=temperature,
+    )
+    return completion.choices[0].text.strip() if completion.choices else ""
+
+def get_summary_diff(new_text, old_text, temperature=0.5):
+    prompt_text = f"You tell me a summary of the changes between new and old text bellow : \n\n\nNEW TEXT: {new_text}\n\nOLD TEXT: {old_text}"
+    completion = client.completions.create(
+        model=model_to_use,
+        prompt=prompt_text,
+        temperature=temperature,
+    )
+    return completion.choices[0].text.strip() if completion.choices else ""
+
+
+
+
+
+
+
 # Load .env file from the current directory
 load_dotenv()
 
@@ -24,6 +73,7 @@ load_dotenv()
 if 'OPENAI_API_KEY' not in os.environ:
     home_dir = os.path.expanduser("~")
     load_dotenv(os.path.join(home_dir, '.env'))
+    
 api_key=os.getenv('OPENAI_API_KEY')
 
 
@@ -71,35 +121,18 @@ with open('jgtsnoter.csv', 'r') as csv_file:
         # If the content has changed or it's a new URL, save the new content and get a new summary
         if url not in data or data[url]['hash'] != content_hash:
             # Save the HTML content into a file
-            with open(f'./_snote_content_cache/{url_hash}.html', 'w') as html_file:
-                html_file.write(soup.prettify())
-
-            # Download images
-            for img in soup.find_all('img'):
-                img_url = urllib.parse.urljoin(url, img['src'])
-                img_filename = hashlib.md5(img_url.encode()).hexdigest()
-                img_response = requests.get(img_url)
-
-                with open(f'./_snote_content_cache/{img_filename}', 'wb') as img_file:
-                    img_file.write(img_response.content)
-
-            # Get a summary of the new content
-            prompt_text = response.text[:5000]
-            completion = client.completions.create(
-                model="text-davinci-002",
-                prompt=prompt_text,
-                temperature=0.3,
-                max_tokens=100
-            )
-
-            new_summary = completion.choices[0].text.strip() if completion.choices else ""
+            save_html_content(url_hash, soup)
+            download_images(soup, url)
+            
+            new_summary = get_summary(response)
 
             # If the URL was processed before, identify changes
             changes = ''
             if url in data:
                 old_summary = data[url]['summary']
-                # This is a simple way to identify changes and might not work well for complex text
-                changes = ' '.join(set(new_summary.split()) - set(old_summary.split()))
+                # Evolved way to identify changes using AI
+                changes = get_summary_diff(old_summary, new_summary)
+                
 
             # Update the data of the URL
             data[url] = {'hash': content_hash, 'summary': new_summary, 'changes': changes, 'title': title}
@@ -111,3 +144,7 @@ with open('jgtsnoter.csv', 'r') as csv_file:
 
     # Call the function from snoterupdate.py
     snoterupdate.generate_markdown(link_titles, data)
+    
+    
+    
+    
