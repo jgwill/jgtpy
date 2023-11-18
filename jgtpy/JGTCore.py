96033c751d0d5442fafd66bd8dae3dd4a22dc367
JGWill Gaia.WSL <jgi@jgwill.com>
2023-11-18 17:11:23 -0500

    v0.1.69

--- jgtpy/JGTCore.py
+++ jgtpy/JGTCore.py
@@ -6,7 +6,7 @@ import pathlib
 from pathlib import Path
 import pandas as pd
 
-__version__ = "0.1.68"
+__version__ = "0.1.69"
 
 from datetime import datetime
 from datetime import timedelta
--- test/batch-cli.sh
+++ test/batch-cli.sh
@@ -1,1 +1,1 @@
-for t in H1 H4 D1 m15 m5 W1;do for i in "EUR/USD" "AUD/USD" "USD/CAD" "GBP/USD" "SPX500";do jgtcli -i "$i" -t "$t" -c 1200 -o;done;done
+for t in H1 H4 D1 m15 m5;do for i in "EUR/USD" "AUD/USD" "USD/CAD" "GBP/USD" "SPX500";do jgtcli -i "$i" -t "$t" -c 4500 -o;done;done
