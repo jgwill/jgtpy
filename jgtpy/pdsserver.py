ed3e3630665aef57d8ddab99810df5d14aad8dc4
JGWill Gaia.WSL <jgi@jgwill.com>
2023-11-27 09:50:32 -0500

    done:First retrieval from file

--- jgtpy/pdsserver.py
+++ jgtpy/pdsserver.py
@@ -1,16 +1,45 @@
 from flask import Flask, jsonify, request
-from jgtpy.JGTPDS import getPH, mk_fn, mk_fullpath,stayConnectedSetter
+from jgtpy import sc,up,h,stay
+
+from jgtpy.JGTPDS import getPH, mk_fn, mk_fullpath,getPH_to_filestore as ph2fs,getPH_from_filestore
+
+#stayConnectedSetter(True)
 
 app = Flask(__name__)
-stayConnectedSetter(True)
+#stayConnectedSetter(True)
+
+
+#@app.route('/stay',method=['GET'])
+#def fetch_stay():
+#    data = request.json
+#    stayConnectedSetter(True)
+#    return '{}'
 
 @app.route('/getPH', methods=['POST'])
 def fetch_getPH():
     data = request.json
     instrument = data['instrument']
     timeframe = data['timeframe']
-    result = getPH(instrument, timeframe)
-    return jsonify(result)  # Assuming the result can be serialized into JSON
+    #result = h(instrument, timeframe)
+    df = getPH_from_filestore(instrument, timeframe)
+    result = df.to_csv()
+    print(result)
+    #return jsonify(result)  # Assuming the result can be serialized into JSON
+    return result  # Assuming the result can be serialized into JSON
+
+
+@app.route('/getPH_from_filestore', methods=['POST'])
+def fetch_getPH_from_filestore():
+    data = request.json
+    instrument = data['instrument']
+    timeframe = data['timeframe']
+    quiet = data.get('quiet', True)  # Optional parameter, defaults to True
+    compressed = data.get('compressed', False)  # Optional parameter, defaults to False
+    with_index = data.get('with_index', True)  # Optional parameter, defaults to True
+    df = getPH_from_filestore(instrument, timeframe, quiet, compressed, with_index)
+    return df.to_json(orient='split')
+
+
 
 @app.route('/mk_fn', methods=['GET'])
 def fetch_mk_fn():
--- pdsserver-client-test.py
+++ pdsserver-client-test.py
@@ -1,9 +1,25 @@
 import requests
 import json
 
-response = requests.post('http://localhost:5000/getPH', json={'instrument': 'EUR/USD', 'timeframe': 'm1'})
-print(response.json())
+response = requests.post('http://localhost:5000/getPH', json={'instrument': 'EUR/USD', 'timeframe': 'H4'})
+#print(response.json())
+print(response.text)
 
+print("mk_fn")
 response = requests.get('http://localhost:5000/mk_fn', params={'instrument': 'EUR/USD', 'timeframe': 'm1', 'ext': 'csv'})
 print(response.json())
 
+print("------------------")
+print("---- from file store route ---")
+
+
+import pandas as pd
+
+response = requests.post('http://localhost:5000/getPH_from_filestore',
+                         json={'instrument': 'EUR/USD',
+                               'timeframe': 'H4',
+                               'quiet': True,
+                               'compressed': False,
+                               'with_index': True})
+df = pd.read_json(response.text, orient='split')
+print(df)
