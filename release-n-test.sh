7a04438bf211d72e75bd6d94e26e3ff4151b61cf
JGWill Gaia.WSL <jgi@jgwill.com>
2023-11-27 11:46:35 -0500

    script

--- release-n-test.sh
+++ release-n-test.sh
@@ -1,4 +1,6 @@
 
+. scripts/version-patcher.sh
 cversion=$(cat pyproject.toml |tr '"' " " |awk '/version/ {print $3}')
 git commit . -m "v$cversion" && git tag "$cversion" && git push --tags && git push 
+
 make dist && twine upload dist/* && sleep 29 &&  . pypi-conda-gaia-env.sh
