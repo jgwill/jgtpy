0d52a60eb4c0707f31f31ed9ff90180a595ad11b
JGWill Gaia.WSL <jgi@jgwill.com>
2023-12-08 18:58:43 -0500

    add:dividers

--- jgtpy/jgtwslhelper.py
+++ jgtpy/jgtwslhelper.py
@@ -32,6 +32,8 @@ def jgtfxcli_wsl(instrument, timeframe, quote_count,cli_path="", verbose_level=0
     bash_command_to_run = f"pwd;{cli_path} -i \"{instrument}\" -t \"{timeframe}\" -c {quote_count} -o -v {verbose_level}"
     return pwsd_wsl_run_command(bash_command_to_run)
 
+def jgtfxcli(instrument, timeframe, quote_count,cli_path="", verbose_level=0):
+    return jgtfxcli_wsl(instrument,timeframe,quote_count,cli_path,verbose_level)
 
 def wsl_cd(directory):
     # Define the command to be executed
@@ -65,3 +67,28 @@ def execute_wsl_command_v1_with_cd(directory, command_to_execute):
     # Print the error (if any)
     if result.stderr:
         print("Error:", result.stderr.decode("utf-8"))
+
+
+
+# Define the dividers for each timeframe
+timeframe_dividers = {
+    "m1": 0.0166,
+    "mi1": 0.0166,
+    "m5": 0.8,
+    "m15": 0.25,
+    "m30": 0.5,
+    "H1": 1,
+    "H2": 2,
+    "H3": 3,
+    "H4": 4,
+    "H5": 5,
+    "H6": 6,
+    "H8": 8,
+    "D1": 24,
+    "W1": 110,
+    "M1": 400
+}
+
+#@STCIssue Locks us to H1, should be interactive and receive an input from the user, lower TF
+def get_timeframe_dividers(base_tf="H1"):
+    return timeframe_dividers[base_tf]
