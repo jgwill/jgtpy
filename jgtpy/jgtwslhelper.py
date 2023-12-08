e8adf1ed7dfd62522a567c8bacc669c180b08c3c
JGWill Gaia.WSL <jgi@jgwill.com>
2023-12-08 16:28:52 -0500

    fix

--- jgtpy/jgtwslhelper.py
+++ jgtpy/jgtwslhelper.py
@@ -3,44 +3,65 @@ import sys
 
 import os
 
+
 def pwsd_wsl_run_command(bash_command_to_run):
-    powershell_command = "wsl.exe bash -c \"" + bash_command_to_run + "\""
-    result = subprocess.run(["pwsh.exe", "-Command", powershell_command], stdout=subprocess.PIPE, shell=True)
-    return result.stdout.decode('utf-8')
+    powershell_command = 'wsl.exe bash -c \'' + bash_command_to_run + '\''
+    result = subprocess.run(
+        ["pwsh.exe", "-Command", powershell_command], stdout=subprocess.PIPE, shell=True
+    )
+    return result.stdout.decode("utf-8")
+
+def run(bash_command):
+    return pwsd_wsl_run_command(bash_command)
 
 def jgtfxcli_wsl(cli_path, instrument, timeframe, quote_count, verbose_level):
-  bash_command_to_run = f"pwd;{cli_path} -i '{instrument}' -t '{timeframe}' -c {quote_count} -o -v {verbose_level}"
-  powershell_command = "wsl.exe bash -c \"" + bash_command_to_run + "\""
-  result = subprocess.run(["pwsh.exe", "-Command", powershell_command], stdout=subprocess.PIPE, shell=True)
-  return result.stdout.decode('utf-8')
+    if cli_path == "" or cli_path is None:
+        cli_path = "/home/jgi/.local/bin/jgtfxcli"
+    bash_command_to_run = f"pwd;{cli_path} -i '{instrument}' -t '{timeframe}' -c {quote_count} -o -v {verbose_level}"
+    powershell_command = 'wsl.exe bash -c "' + bash_command_to_run + '"'
+    result = subprocess.run(
+        ["pwsh.exe", "-Command", powershell_command], stdout=subprocess.PIPE, shell=True
+    )
+    return result.stdout.decode("utf-8")
+
+
+def jgtfxcli_wsl2(instrument, timeframe, quote_count,cli_path="", verbose_level=0):
+    if cli_path == "" or cli_path is None:
+        cli_path = '$HOME/.local/bin/jgtfxcli'
+        #cli_path = "/home/jgi/.local/bin/jgtfxcli"
+    bash_command_to_run = f"pwd;{cli_path} -i \"{instrument}\" -t \"{timeframe}\" -c {quote_count} -o -v {verbose_level}"
+    return pwsd_wsl_run_command(bash_command_to_run)
+
 
 def wsl_cd(directory):
     # Define the command to be executed
-    command = ['wsl.exe', 'cd', directory]
+    command = ["wsl.exe", "cd", directory]
 
     # Execute the command
     result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    # Print the output
-    print(result.stdout.decode('utf-8'))
 
     # Print the error (if any)
     if result.stderr:
-        print("Error:", result.stderr.decode('utf-8'))
+        return result.stderr.decode("utf-8")
+    else:
+        # Print the output
+        return result.stdout.decode("utf-8")
+        
 
-
-
+def cd(tpath):
+    wsl_cd(tpath)
 
 def execute_wsl_command_v1_with_cd(directory, command_to_execute):
     # Define the command to be executed
-    command = ['wsl.exe', 'cd', directory, '&&', command_to_execute]
+    command = ["wsl.exe", "cd", directory, "&&", command_to_execute]
 
     # Execute the command
     result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     # Print the output
-    print(result.stdout.decode('utf-8'))
+    print(result.stdout.decode("utf-8"))
 
     # Print the error (if any)
     if result.stderr:
-        print("Error:", result.stderr.decode('utf-8'))
+        print("Error:", result.stderr.decode("utf-8"))
