9bcd866ecaca18a23392ac8009620b378fa342bb
JGWill <jgi@jgwill.com>
2024-02-27 14:58:29 -0500

    upgraded signature

--- jgtpy/JGTIDSRequest.py
+++ jgtpy/JGTIDSRequest.py
@@ -1,16 +1,17 @@
 class JGTIDSRequest:
-    def __init__(self, AOF=False, BigAlligator=False, MFI=False):
+    def __init__(self, AOF=False, BigAlligator=False, MFI=False, GatorOscillator=False,
+                 balligator_period_jaws=89, balligator_period_teeth=55, balligator_period_lips=34,
+                 balligator_shift_jaws=55, balligator_shift_teeth=34, balligator_shift_lips=21,
+                 largest_fractal_period=89):
         self.AOF = AOF
         self.BigAlligator = BigAlligator
         self.MFI = MFI
-        
-        self.GatorOscillator=False
-        
-        self.balligator_period_jaws = 89
-        self.balligator_period_teeth = 55
-        self.balligator_period_lips = 34
-        self.balligator_shift_jaws = 55
-        self.balligator_shift_teeth = 34
-        self.balligator_shift_lips = 21
-        
-        self.largest_fractal_period = 89
+        self.GatorOscillator = GatorOscillator
+        self.balligator_period_jaws = balligator_period_jaws
+        self.balligator_period_teeth = balligator_period_teeth
+        self.balligator_period_lips = balligator_period_lips
+        self.balligator_shift_jaws = balligator_shift_jaws
+        self.balligator_shift_teeth = balligator_shift_teeth
+        self.balligator_shift_lips = balligator_shift_lips
+        self.largest_fractal_period = largest_fractal_period
+
