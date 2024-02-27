d53f07226e3c1e46f1edc7640bb428f1f4b79ac5
JGWill <jgi@jgwill.com>
2024-02-27 15:00:21 -0500

    adD:doc

--- jgtpy/JGTIDSRequest.py
+++ jgtpy/JGTIDSRequest.py
@@ -1,4 +1,33 @@
 class JGTIDSRequest:
+    """
+    Represents a JGT IDS (Indicator Data Structure) request.
+
+    Args:
+        AOF (bool): Flag indicating whether to include AOF (AO Peak V1 (Would be replaced by the scipy.signals peak)) data.
+        BigAlligator (bool): Flag indicating whether to include Big Alligator data.
+        MFI (bool): Flag indicating whether to include MFI (Money Flow Index) data.
+        GatorOscillator (bool): Flag indicating whether to include Gator Oscillator data.
+        balligator_period_jaws (int): Period for the jaws line of the Alligator indicator.
+        balligator_period_teeth (int): Period for the teeth line of the Alligator indicator.
+        balligator_period_lips (int): Period for the lips line of the Alligator indicator.
+        balligator_shift_jaws (int): Shift for the jaws line of the Alligator indicator.
+        balligator_shift_teeth (int): Shift for the teeth line of the Alligator indicator.
+        balligator_shift_lips (int): Shift for the lips line of the Alligator indicator.
+        largest_fractal_period (int): Period for identifying the largest fractal.
+
+    Attributes:
+        AOF (bool): Flag indicating whether to include AOF
+        BigAlligator (bool): Flag indicating whether to include Big Alligator data.
+        MFI (bool): Flag indicating whether to include MFI (Market Facilitation Index) data.
+        GatorOscillator (bool): Flag indicating whether to include Gator Oscillator data.
+        balligator_period_jaws (int): Period for the jaws line of the Alligator indicator.
+        balligator_period_teeth (int): Period for the teeth line of the Alligator indicator.
+        balligator_period_lips (int): Period for the lips line of the Alligator indicator.
+        balligator_shift_jaws (int): Shift for the jaws line of the Alligator indicator.
+        balligator_shift_teeth (int): Shift for the teeth line of the Alligator indicator.
+        balligator_shift_lips (int): Shift for the lips line of the Alligator indicator.
+        largest_fractal_period (int): Period for identifying the largest fractal.
+    """
     def __init__(self, AOF=False, BigAlligator=False, MFI=False, GatorOscillator=False,
                  balligator_period_jaws=89, balligator_period_teeth=55, balligator_period_lips=34,
                  balligator_shift_jaws=55, balligator_shift_teeth=34, balligator_shift_lips=21,
