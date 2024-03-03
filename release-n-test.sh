170fcbbf88bde8c3ef8b3f55517df90d92fd9ea1
JGWill <jgi@jgwill.com>
2024-03-02 23:18:06 -0500

    fix

--- release-n-test.sh
+++ release-n-test.sh
@@ -4,3 +4,4 @@ cversion=$(cat pyproject.toml |tr '"' " " |awk '/version/ {print $3}')
 git commit . -m "v$cversion" && git tag "$cversion" && git push --tags && git push 
 
 make dist && twine upload dist/* #&& sleep 32 &&  . pypi-conda-gaia-env.sh
+echo "pip install -U jgtpy==$cversion"
