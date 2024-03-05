487f4dece5e45401801524e337a0bc361a7f62a8
JGWill <jgi@jgwill.com>
2024-03-04 20:31:29 -0500

    v0.4.0

--- batchJGTCDS.py
+++ batchJGTCDS.py
@@ -4,7 +4,7 @@ import jgtpy.JGTCDS as cds
 import os
 instrument="AUD/USD"
 timeframe = "D1"
-c=cds.create(instrument,timeframe,nb2retrieve=3000,stayConnected=False,quiet=True)
+c=cds.create(instrument,timeframe,quiet=True)
 print(c)
 
 # Define the file path based on the environment variable or local path
--- release-n-test.sh
+++ release-n-test.sh
@@ -1,7 +1,7 @@
 
 . scripts/version-patcher.sh
 cversion=$(cat pyproject.toml |tr '"' " " |awk '/version/ {print $3}')
-git commit . -m "v$cversion" && git tag "$cversion" && git push --tags && git push 
+git commit . -m "v$cversion";git tag "$cversion" && git push --tags && git push 
 
 make dist && twine upload dist/* #&& sleep 32 &&  . pypi-conda-gaia-env.sh
 echo "pip install -U jgtpy==$cversion"
