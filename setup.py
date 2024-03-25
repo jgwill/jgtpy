e6f09d0c1142c5988539d9f8c0cb15b9023e1aef
JGWill <jgi@jgwill.com>
2024-03-25 18:01:35 -0400

    dep

--- setup.py
+++ setup.py
@@ -10,15 +10,14 @@ from jgtpy import __version__ as version
 INSTALL_REQUIRES = [
     "pandas>=0.25.1",
     "python-dotenv>=0.19.2",
-    #'kaleido>=0.2.1',
+    "jgtapy>=1.9.20",
+    "jgtutils>=0.1.41",
+    "dropbox>=11.36.2",
+    "tlid>=0.1.13",
+    "scipy>=1.9.1",
     "matplotlib>=3.3.1",
     "plotly>=5.18.0",
-    "jgtapy>=1.9.20",
-    "jgtutils>=0.1.35",
-    "dropbox>=11.36.2",
     "mplfinance>=0.12.10b0",
-    "tlid>=0.1.13",
-    "scipy>=1.7.3",
     "panel>=1.2.3",
     "tomli>=1.2.3",
 ]
@@ -49,11 +48,13 @@ setup(
     author="GUillaume Isabelle",
     author_email="jgi@jgwill.com",
     url="https://github.com/jgwill/jgtpy",
-    packages=find_packages(include=["jgtpy","test-*.py"], exclude=["test*log","*test*csv","*test*png"]),
+    packages=find_packages(
+        include=["jgtpy", "test-*.py"], exclude=["test*log", "*test*csv", "*test*png"]
+    ),
     # packages=find_packages(include=['jgtpy', 'jgtpy.forexconnect', 'jgtpy.forexconnect.lib', 'jgtpy.forexconnect.lib.windows', 'jgtpy.forexconnect.lib.linux'], exclude=['*test*']),
     install_requires=INSTALL_REQUIRES,
     entry_points={
-        "console_scripts": ["jgtcli=jgtpy.jgtcli:main","jgtmksg=jgtpy.JGTMKSG:main"],
+        "console_scripts": ["jgtcli=jgtpy.jgtcli:main", "jgtmksg=jgtpy.JGTMKSG:main"],
     },
     extras_require={
         "dev": (EXTRAS_DEV_LINT + EXTRAS_DEV_TEST + EXTRAS_DEV_DOCS),
