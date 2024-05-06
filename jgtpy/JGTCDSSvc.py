391f0116fd837202ed9ebc4e9ee8c4dda3709e03
JGWill <jgi@jgwill.com>
2024-05-06 11:30:24 -0400

    @STCIssue Parse the CDS and get Valid signal (not fully implemented)

---
+++ jgtpy/JGTCDSSvc.py
@@ -0,0 +1,11 @@
+#import JGTCDS as cds
+
+#todo expose only the required functions to run CDS
+
+## Requirement:   
+
+"""
+* Generate the CDS to file
+* --@STCIssue Parse the CDS and get Valid signal (not fully implemented)
+
+"""
