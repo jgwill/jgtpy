05c2f1646da515b2fabccb4b84e446ad9a0bc3cc
JGWill <jgi@jgwill.com>
2024-08-15 19:55:06 -0400

    dev

--- release-n-test.sh
+++ release-n-test.sh
@@ -1,6 +1,6 @@
 git commit package.json pyproject.toml jgtpy/__init__.py -m bump &>/dev/null
-. bump_jgtutils.sh
 pip install -U jgtutils&>/dev/null && echo "jgtutils upgraded" || echo "jgtutils not installed"
+. bump_jgtutils.sh
 . scripts/version-patcher.sh
 cversion=$(cat pyproject.toml |tr '"' " " |awk '/version/ {print $3}')
 git commit . -m "v$cversion";git tag "$cversion" && git push --tags && git push 
