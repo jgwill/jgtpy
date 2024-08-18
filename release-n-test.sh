adfb67221b48eee664a84d3818e124dc7eb71064
JGWill <jgi@jgwill.com>
2024-08-18 04:32:43 -0400

    fix

--- release-n-test.sh
+++ release-n-test.sh
@@ -1,6 +1,7 @@
 git commit package.json pyproject.toml jgtpy/__init__.py -m bump &>/dev/null
 
-. /opt/binscripts/load.sh && _bump_jgtutils
+. /opt/binscripts/load.sh 
+make bump_jgtutils
 . scripts/version-patcher.sh
 cversion=$(cat pyproject.toml |tr '"' " " |awk '/version/ {print $3}')
 git commit . -m "v$cversion";git tag "$cversion" && git push --tags && git push 
